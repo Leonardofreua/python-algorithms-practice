4fe3be5328ae9074791c5e8308cb77008f9305a1
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-11-05 18:01:31 -0300

    Add array manipulation.

---
+++ array_manipulation/_array.py
@@ -0,0 +1,275 @@
+from typing import TypeVar
+
+INITIAL_SIZE = 0
+MIN_INITIAL_CAPACITY = 1
+MIN_REAL_CAPACITY = 16
+GROWTH_FACTOR = 2
+SHRINK_FACTOR = 4
+
+
+T = TypeVar("T", int, str, None)
+
+INDEX_OUT_OF_RANGE_ERROR = "Index out of range."
+NOT_MATCH_VALUE_FOUND_ERROR = "No match value found."
+
+
+class Array:
+    def __init__(self, capacity: int = MIN_INITIAL_CAPACITY) -> None:
+        """
+        Creates a new array according to the provided capacity.
+
+        About: An array is a contiguous area of memory consisting of equal-size
+        elements indexed by contiguous integers.
+
+        - Complexity:
+            * Constant-time access to any element - O(1);
+            * Constant-time to add/remove at the end - O(1);
+            * Linear time to add/remove at an arbitrary location - O(n).
+        """
+        self._size: int = INITIAL_SIZE
+        self._capacity: int = self._set_capacity(capacity)
+        self.data: list[T] = []
+
+    def append(self, value: T) -> None:
+        """
+        Add an item to the end of the list.
+
+        - Complexity: O(1)
+
+        Args:
+            value: The value that will be add.
+        """
+        self._upsize_capacity()
+        if self.is_empty():
+            self.data = [value]
+        else:
+            self.data += [value]
+        self._size += 1
+
+    def insert(self, index: int, value: T) -> None:
+        """
+        Insert an item at a given position.
+
+            * [index = 0]: Insert the item at the beginning - Complexity: O(n);\n
+            * [index > current size or index < 0]: Insert the item at the end
+                (is equivalent to append(value)) - Complexity: O(1);\n
+            * [index < current size or any other numbers]: Insert the item among
+                other elements - Complexity: O(n).
+        --------
+
+        Args:
+            index: The index where you want to insert the value.
+            value: The value that will be insert.
+
+        Raises:
+            ValueError: If the index is not an Integer
+        """
+        self._validate_index_type(index)
+
+        self._upsize_capacity()
+        if index == INITIAL_SIZE:  # Insert the element at the beginning
+            shifted_array = [value]
+            for item in self.data:
+                shifted_array += [item]
+            self.data = shifted_array
+            self._size += 1
+        elif (
+            index > self._size or index < INITIAL_SIZE
+        ):  # Insert the element at the end
+            self.append(value)
+        else:  # Insert the element among other elements
+            left_content = self.data[:index]
+            right_content = self.data[index:]
+
+            left_content += [value]
+            for item in right_content:
+                left_content += [item]
+
+            self.data = left_content
+            self._size += 1
+
+    def at(self, index: int) -> T:
+        """
+        Returns item at given index.
+
+        - Complexity: O(1)
+
+        Args:
+            index: Index where the item is found.
+
+        Raises:
+            - IndexError: If index out of range.
+            - ValueError: If the index is not an Integer.
+
+        Return:
+            T: The found item
+        """
+        self._validate_index_type(index)
+
+        if index < INITIAL_SIZE or index > self._size - 1:
+            raise IndexError(INDEX_OUT_OF_RANGE_ERROR)
+
+        return self.data[index]
+
+    def pop(self, index: int = None) -> T:
+        """
+        Remove a value at the end, and return it.
+
+        - Complexity:
+            * O(1) if the index is at the end.
+            * O(n) from specific index.
+
+        Args:
+            index: Index where the item is found.
+
+        Raises:
+            - IndexError:
+                * If there is nothing in the array;
+                * And if index is greater than size.
+            - ValueError: If the index is not an integer.
+
+        Return:
+            T: The removed value
+        """
+        if index is not None:
+            self._validate_index_type(index)
+
+        if self.is_empty() or index is not None and index > self._size:
+            raise IndexError(INDEX_OUT_OF_RANGE_ERROR)
+
+        target_value: T = None
+        if index is None or index < INITIAL_SIZE or index == self._size - 1:
+            self._downsize_capacity()
+            target_value = self.data[-1]
+            del self.data[-1]
+            self._size -= 1
+        else:
+            # Searching...
+            for value in self.data:
+                if value == self.data[index]:
+                    target_value = value
+                    self._downsize_capacity()
+                    self._reorganize(index)
+
+                    del self.data[-1]
+                    self._size -= 1
+                    break
+            else:
+                raise IndexError(INDEX_OUT_OF_RANGE_ERROR)
+
+        return target_value
+
+    def remove(self, target_value: T) -> None:
+        """
+        Remove the first item from the array that is equal to the target value.
+
+        Complexity: O(n)
+
+        Args:
+            target_value: value you want to remove.
+
+        Raises:
+            ValueError:
+                * if no matching value is found.
+        """
+        index_found = self.find(target_value)
+        self._downsize_capacity()
+        self._reorganize(index_found)
+        del self.data[-1]
+        self._size -= 1
+
+    def remove_all(self, target_values: list[T]) -> None:
+        """
+        Remove values from the array that are equal to the list of target values.
+
+        Complexity: O(n)
+
+        Args:
+            target_values: list of values you want to remove.
+
+        Raises:
+            ValueError: If no matching value is found.
+        """
+        self._downsize_capacity()
+        for target in target_values:
+            index_found = self.find(target)
+            self._reorganize(index_found)
+            del self.data[-1]
+            self._size -= 1
+
+    def find(self, value: T) -> int:
+        """
+        Search for the given value and returns its index.
+
+        - Complexity: O(n)
+
+        Args:
+            value: value you want to find.
+
+        Raises:
+            ValueError: If the value is not found.
+        """
+        for index, item in enumerate(self.data):
+            if item == value:
+                break
+        else:
+            raise ValueError(NOT_MATCH_VALUE_FOUND_ERROR)
+        return index
+
+    def is_empty(self) -> bool:
+        """Check if the array is empty."""
+        return self._size == INITIAL_SIZE
+
+    def size(self) -> int:
+        return self._size
+
+    def capacity(self) -> int:
+        return self._capacity
+
+    def _reorganize(self, target_index: int) -> None:
+        for i in range(target_index, self._size - 1):
+            self.data[i] = self.data[i + 1]
+
+    @staticmethod
+    def _set_capacity(capacity: int) -> int:
+        """
+        Arrays will be created with a minimium of 16 bits (2 bytes) and
+        a growth factor of 2.
+
+        Examples:
+            * capacity = 8 ->  Real capacity: 16 bits (2 bytes);
+            * capacity = 9 ->  Real capacity: 32 bits (4 bytes);
+            * capacity = 17 -> Real capacity: 64 bits (8 bytes);
+            * ....
+        """
+        if capacity < MIN_INITIAL_CAPACITY:
+            raise ValueError(
+                f"The minimium initial capacity must be greater than or equal to {MIN_INITIAL_CAPACITY}"
+            )
+
+        real_capacity = MIN_REAL_CAPACITY
+        while capacity > real_capacity // GROWTH_FACTOR:
+            real_capacity *= GROWTH_FACTOR
+
+        return real_capacity
+
+    def _upsize_capacity(self) -> None:
+        if self._size == self._capacity:
+            old_capacity = self._capacity
+            self._capacity = self._set_capacity(old_capacity)
+
+    def _downsize_capacity(self) -> None:
+        if self._size < self._capacity // SHRINK_FACTOR:
+            old_capacity = self._capacity
+            new_capacity = self._capacity // GROWTH_FACTOR
+
+            if new_capacity < MIN_REAL_CAPACITY:
+                new_capacity = MIN_REAL_CAPACITY
+
+            if new_capacity != old_capacity:
+                self._capacity = new_capacity
+
+    @staticmethod
+    def _validate_index_type(index: int) -> None:
+        if not isinstance(index, int):
+            raise ValueError("Index must be a integer!")
---
+++ array_manipulation/test_arrays.py
@@ -0,0 +1,436 @@
+from unittest import TestCase
+
+from _array import Array
+
+
+class TestArrays(TestCase):
+    def test_new_array(self) -> None:
+        new_array = Array(10)
+        self.assertEqual(new_array.size(), 0)
+        self.assertEqual(new_array.capacity(), 32)
+
+    def test_integer_append(self) -> None:
+        new_array = Array()
+
+        for index, value in enumerate(range(0, 20)):
+            new_array.append(value)
+            self.assertEqual(new_array.data[index], value)
+            self.assertEqual(new_array.size(), index + 1)
+
+    def test_string_append(self) -> None:
+        new_array = Array()
+
+        for index, value in enumerate(["hi", "olá", "salut", "Guten Tag", "Anyoung"]):
+            new_array.append(value)
+            self.assertEqual(new_array.data[index], value)
+            self.assertEqual(new_array.size(), index + 1)
+
+    def test_insert_integer_at_the_beginning(self) -> None:
+        new_array = Array()
+
+        # [0, 1, 2]
+        for value in range(3):
+            new_array.append(value)
+
+        new_array.insert(0, 3)
+        self.assertEqual(new_array.data[0], 3)
+        self.assertEqual(new_array.size(), 4)
+
+        new_array.insert(0, 4)
+        self.assertEqual(new_array.data[0], 4)
+        self.assertEqual(new_array.size(), 5)
+
+    def test_insert_string_at_the_beginning(self) -> None:
+        new_array = Array()
+
+        for value in ["hi", "olá", "salut"]:
+            new_array.append(value)
+
+        new_array.insert(0, "Guten Tag")
+        self.assertEqual(new_array.data[0], "Guten Tag")
+        self.assertEqual(new_array.size(), 4)
+
+        new_array.insert(0, "Anyoung")
+        self.assertEqual(new_array.data[0], "Anyoung")
+        self.assertEqual(new_array.size(), 5)
+
+    def test_insert_integer_at_the_end(self) -> None:
+        new_array = Array()
+
+        # [0, 1, 2]
+        for value in range(3):
+            new_array.append(value)
+
+        # index > size
+        new_array.insert(new_array.size() + 1, 4)
+        self.assertEqual(new_array.data[-1], 4)
+
+        # index < 0
+        new_array.insert(-1, 5)
+        self.assertEqual(new_array.data[-1], 5)
+
+        self.assertEqual(new_array.size(), 5)
+
+    def test_insert_string_at_the_end(self) -> None:
+        new_array = Array()
+
+        for value in ["hi", "olá", "salut"]:
+            new_array.append(value)
+
+        # index > size
+        new_array.insert(new_array.size() + 1, "Guten Tag")
+        self.assertEqual(new_array.data[-1], "Guten Tag")
+
+        # index < 0
+        new_array.insert(-1, "Anyoung")
+        self.assertEqual(new_array.data[-1], "Anyoung")
+
+        self.assertEqual(new_array.size(), 5)
+
+    def test_insert_integer_at_specific_position(self) -> None:
+        new_array = Array()
+
+        # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for value in range(10):
+            new_array.append(value)
+
+        # In place of the 5
+        new_array.insert(5, 88)
+        # [0, 1, 2, 3, 4, 88, 5, 6, 7, 8, 9]
+        self.assertEqual(new_array.data[5], 88)
+
+        # In place of the 9
+        new_array.insert(10, 10)
+        # [0, 1, 2, 3, 4, 88, 5, 6, 7, 8, 10, 9]
+        self.assertEqual(new_array.data[10], 10)
+
+        self.assertEqual(new_array.size(), 12)
+
+    def test_insert_string_at_specific_position(self) -> None:
+        new_array = Array()
+
+        for value in ["hi", "olá", "salut", "Guten Tag", "Anyoung", "Goddag"]:
+            new_array.append(value)
+
+        # In place of the Guten Tag
+        new_array.insert(3, "Ahlan")
+        # ["hi", "olá", "salut", "Ahlan", "Guten Tag", "Anyoung", "Goddag"]
+        self.assertEqual(new_array.data[3], "Ahlan")
+
+        # In place of the Goddag
+        new_array.insert(6, "Yassou")
+        # ["hi", "olá", "salut", "Ahlan", "Guten Tag", "Anyoung", "Yassou", "Goddag"]
+        self.assertEqual(new_array.data[6], "Yassou")
+
+        self.assertEqual(new_array.size(), 8)
+
+    def test_raise_ValueError_when_insert_with_an_invalid_index_type(self) -> None:
+        new_array = Array()
+
+        with self.assertRaises(ValueError):
+            new_array.insert("i", 1)
+
+    def test_get_value_from_specific_index(self) -> None:
+        new_array = Array()
+
+        for value in range(20):
+            new_array.append(value)
+
+        self.assertEqual(new_array.at(19), 19)
+
+    def test_raise_IndexError_getting_value_list_out_of_range(self) -> None:
+        new_array = Array()
+
+        with self.assertRaises(IndexError):
+            new_array.at(0)
+
+    def test_raise_ValueError_getting_value_with_an_invalid_index_type(self) -> None:
+        new_array = Array()
+
+        with self.assertRaises(ValueError):
+            new_array.at("i")
+
+    def test_pop_integer_value_at_the_end(self) -> None:
+        new_array = Array()
+
+        # [0, 1, 2]
+        for value in range(3):
+            new_array.append(value)
+
+        self.assertEqual(new_array.size(), 3)
+
+        p1 = new_array.pop()
+        # [0, 1]
+        self.assertEqual(p1, 2)
+        self.assertEqual(new_array.size(), 2)
+        self.assertEqual(new_array.data[-1], 1)
+
+        p2 = new_array.pop()
+        # [0]
+        self.assertEqual(p2, 1)
+        self.assertEqual(new_array.size(), 1)
+        self.assertEqual(new_array.data[-1], 0)
+
+    def test_pop_string_value_at_the_end(self) -> None:
+        new_array = Array()
+
+        for value in ["hi", "olá", "salut", "Guten Tag"]:
+            new_array.append(value)
+
+        self.assertEqual(new_array.size(), 4)
+
+        p1 = new_array.pop()
+        # ["hi", "olá", "salut"]
+        self.assertEqual(p1, "Guten Tag")
+        self.assertEqual(new_array.size(), 3)
+
+        p2 = new_array.pop()
+        # ["hi", "olá"]
+        self.assertEqual(p2, "salut")
+        self.assertEqual(new_array.size(), 2)
+
+    def test_raise_IndexError_when_try_pop_value_into_an_empty_array(self) -> None:
+        new_array = Array()
+
+        with self.assertRaises(IndexError):
+            new_array.pop()
+
+    def test_raise_IndexError_when_try_pop_value_with_an_index_greates_array_size(
+        self,
+    ) -> None:
+        new_array = Array()
+
+        new_array.append(1)
+        with self.assertRaises(IndexError):
+            new_array.pop(33)
+
+    def test_raise_ValueError_when_try_pop_value_with_a_non_integer_index(
+        self,
+    ) -> None:
+        new_array = Array()
+
+        new_array.append(1)
+        with self.assertRaises(ValueError):
+            new_array.pop("i")
+
+    def test_pop_integer_value_from_specific_index(self) -> None:
+        new_array = Array()
+
+        # [0, 1, 2, 3, 4]
+        for value in range(5):
+            new_array.append(value)
+
+        p1 = new_array.pop(3)
+        # [0, 1, 2, 4]
+        self.assertEqual(p1, 3)
+        self.assertEqual(new_array.size(), 4)
+        self.assertEqual(new_array.data[-1], 4)
+
+        p2 = new_array.pop(2)
+        # [0, 1, 4]
+        self.assertEqual(p2, 2)
+        self.assertEqual(new_array.size(), 3)
+        self.assertEqual(new_array.data[-1], 4)
+
+        p3 = new_array.pop(1)
+        # [0, 4]
+        self.assertEqual(p3, 1)
+        self.assertEqual(new_array.size(), 2)
+        self.assertEqual(new_array.data[-1], 4)
+
+        p4 = new_array.pop(0)
+        # [4]
+        self.assertEqual(p4, 0)
+        self.assertEqual(new_array.size(), 1)
+        self.assertEqual(new_array.data[-1], 4)
+
+        p5 = new_array.pop(0)
+        # []
+        self.assertEqual(p5, 4)
+        self.assertEqual(new_array.size(), 0)
+
+        self.assertTrue(new_array.is_empty())
+
+    def test_pop_string_value_from_specific_index(self) -> None:
+        new_array = Array()
+
+        for value in ["hi", "olá", "salut", "Guten Tag"]:
+            new_array.append(value)
+
+        self.assertEqual(new_array.size(), 4)
+
+        p1 = new_array.pop(2)
+        # ["hi", "olá", "Guten Tag"]
+        self.assertEqual(p1, "salut")
+        self.assertEqual(new_array.size(), 3)
+
+        p2 = new_array.pop(1)
+        # ["hi", "Guten Tag"]
+        self.assertEqual(p2, "olá")
+        self.assertEqual(new_array.size(), 2)
+
+        p4 = new_array.pop(0)
+        # ["Guten Tag"]
+        self.assertEqual(p4, "hi")
+        self.assertEqual(new_array.size(), 1)
+
+        p4 = new_array.pop(0)
+        # []
+        self.assertEqual(p4, "Guten Tag")
+        self.assertEqual(new_array.size(), 0)
+
+        self.assertTrue(new_array.is_empty())
+
+    def test_raise_ValueError_when_try_remove_a_non_existent_value(self) -> None:
+        new_array = Array()
+
+        new_array.append(1)
+        with self.assertRaises(ValueError):
+            new_array.remove(33)
+
+        with self.assertRaises(ValueError):
+            new_array.remove("hi")
+
+    def test_remove_integer_value(self) -> None:
+        new_array = Array()
+
+        for value in [1, 2, 3, 4, 4, 5, 6, 6]:
+            new_array.append(value)
+
+        new_array.remove(4)
+        self.assertListEqual(new_array.data, [1, 2, 3, 4, 5, 6, 6])
+        self.assertEqual(new_array.size(), 7)
+
+        new_array.remove(6)
+        self.assertListEqual(new_array.data, [1, 2, 3, 4, 5, 6])
+        self.assertEqual(new_array.size(), 6)
+
+        new_array.remove(3)
+        self.assertListEqual(new_array.data, [1, 2, 4, 5, 6])
+        self.assertEqual(new_array.size(), 5)
+
+    def test_remove_string_value(self) -> None:
+        new_array = Array()
+
+        for value in [
+            "hi",
+            "olá",
+            "salut",
+            "Guten Tag",
+            "Guten Tag",
+            "Anyoung",
+            "Goddag",
+            "Goddag",
+        ]:
+            new_array.append(value)
+
+        new_array.remove("olá")
+        self.assertListEqual(
+            new_array.data,
+            ["hi", "salut", "Guten Tag", "Guten Tag", "Anyoung", "Goddag", "Goddag"],
+        )
+        self.assertEqual(new_array.size(), 7)
+
+        new_array.remove("Goddag")
+        self.assertListEqual(
+            new_array.data,
+            ["hi", "salut", "Guten Tag", "Guten Tag", "Anyoung", "Goddag"],
+        )
+        self.assertEqual(new_array.size(), 6)
+
+        new_array.remove("Guten Tag")
+        self.assertListEqual(
+            new_array.data, ["hi", "salut", "Guten Tag", "Anyoung", "Goddag"]
+        )
+        self.assertEqual(new_array.size(), 5)
+
+    def test_raise_ValueError_when_try_remove_multiple_non_existent_values(
+        self,
+    ) -> None:
+        new_array = Array()
+
+        new_array.append(1)
+        with self.assertRaises(ValueError):
+            new_array.remove_all([33, "hi"])
+
+    def test_remove_multiple_integer_values(self) -> None:
+        new_array = Array()
+
+        for value in [1, 2, 3, 4, 4, 5, 6, 6]:
+            new_array.append(value)
+
+        new_array.remove_all([4, 4])
+        self.assertListEqual(new_array.data, [1, 2, 3, 5, 6, 6])
+        self.assertEqual(new_array.size(), 6)
+
+        new_array.remove_all([6, 6])
+        self.assertListEqual(new_array.data, [1, 2, 3, 5])
+        self.assertEqual(new_array.size(), 4)
+
+        new_array.remove_all([3, 5])
+        self.assertListEqual(new_array.data, [1, 2])
+        self.assertEqual(new_array.size(), 2)
+
+    def test_remove_multiple_string_value(self) -> None:
+        new_array = Array()
+
+        for value in [
+            "hi",
+            "olá",
+            "salut",
+            "Guten Tag",
+            "Guten Tag",
+            "Anyoung",
+            "Goddag",
+            "Goddag",
+        ]:
+            new_array.append(value)
+
+        new_array.remove_all(["olá", "hi"])
+        self.assertListEqual(
+            new_array.data,
+            ["salut", "Guten Tag", "Guten Tag", "Anyoung", "Goddag", "Goddag"],
+        )
+        self.assertEqual(new_array.size(), 6)
+
+        new_array.remove_all(["Goddag", "Goddag"])
+        self.assertListEqual(
+            new_array.data,
+            ["salut", "Guten Tag", "Guten Tag", "Anyoung"],
+        )
+        self.assertEqual(new_array.size(), 4)
+
+        new_array.remove_all(["Guten Tag", "Guten Tag"])
+        self.assertListEqual(new_array.data, ["salut", "Anyoung"])
+        self.assertEqual(new_array.size(), 2)
+
+    def test_raise_ValueError_when_try_find_a_non_existent_value(self) -> None:
+        new_array = Array()
+
+        with self.assertRaises(ValueError):
+            new_array.find(1)
+
+        with self.assertRaises(ValueError):
+            new_array.find("i")
+
+    def test_find_integer_value(self) -> None:
+        new_array = Array()
+
+        for value in [100, 44, 78, 22, 37]:
+            new_array.append(value)
+
+        for index, value in enumerate(new_array.data):
+            self.assertEqual(new_array.find(value), index)
+
+    def test_find_string_value(self) -> None:
+        new_array = Array()
+
+        for value in ["a", "b", "c", "z", "Y", "w"]:
+            new_array.append(value)
+
+        for index, value in enumerate(new_array.data):
+            self.assertEqual(new_array.find(value), index)
+
+
+if __name__ == "__main__":
+    unittest.main()
